05c2b558c11e2617e3c012ea7e6efd10a65d2243
Zhou <1174714790@qq.com>
2023-05-15 00:47:44 +0800

    音乐播放器

---
+++ src/component/RPi/Music.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import ReactAplayer from "react-aplayer"
+const Music = ({audio}) => {
+    const onPlay = () => {
+        console.log('on play');
+    };
+
+    const onPause = () => {
+        console.log('on pause');
+    };
+
+    const onInit = ap => {
+        console.log("init")
+        // ap = ap;
+    };
+
+    return (
+        <>
+            <ReactAplayer
+                audio={audio}
+                onInit={onInit}
+                onPlay={onPlay}
+                onPause={onPause}
+            />
+        </>
+    )
+}
+
+export default Music
--- src/component/RPi/RPi.js
+++ src/component/RPi/RPi.js
@@ -1,74 +1,70 @@
-import * as THREE from 'three';
-import React, { Component } from 'react'
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import React from 'react'
+import {Button, Table} from '@douyinfe/semi-ui';
+import {IconPlay} from "@douyinfe/semi-icons";
+import "./RPi.css"
+import Music from "./Music";
 
-class RPi extends Component {
-    componentDidMount(){
-        // this.init()
-        // this.animate()
+const RPi = () => {
+    const [audio, setAudio] = React.useState([])
+
+    const data = [
+        {
+            key: '1',
+            name: 'twice.mp3',
+            size: '2M',
+            updateTime: '2023-01-12 05:13',
+            url: "/resources/audio/twice.mp3"
+        },
+        {
+            key: '2',
+            name: '刘耀文.mp3',
+            size: '2M',
+            updateTime: '2020-01-17 05:31',
+            url: "/resources/audio/刘耀文.mp3"
+        }
+    ];
+
+    const click_play = (record) => {
+        console.log(record.name)
+        console.log(record.url)
+        const new_audio = {
+            name: record.name,
+            artist: 'Yourself',
+            url: record.url
+        }
+        setAudio([].concat(new_audio))
     }
 
-    init = () => {
-        const scene = new THREE.Scene()
-        // scene.background = new THREE.Color( 0xf0f0f0 );
-        this.scene = scene
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
-        this.camera = camera
-        const renderer = new THREE.WebGLRenderer()
-        this.renderer = renderer
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Size',
+            dataIndex: 'size',
+        },
+        {
+            title: 'Update Time',
+            dataIndex: 'updateTime',
+        },
+        {
+            title: 'Play',
+            dataIndex: 'play',
+            render: (text, record) => (
+                <Button icon={<IconPlay/>} theme="borderless" onClick={() => click_play(record)} />
+            ),
+        }
+    ];
 
-        const loader = new GLTFLoader();
-
-        loader.load( '/resources/Model/raspberry_p4b_v001.gltf', function ( gltf ) {
-            scene.add(gltf.scene);
-        }, undefined, function ( error ) {
-            console.error(error);
-        } );
-
-        // const geometry = new THREE.BoxGeometry(1,1,1)
-        // const material = new THREE.MeshPhongMaterial({color:'red'})
-        // const mesh = new THREE.Mesh(geometry, material)
-        // this.mesh = mesh
-        // scene.add(mesh)
-
-        camera.position.y = 300;
-        camera.position.set(3,3,3)
-        // camera.lookAt(this.rpi_model.position)
-
-        // const pointLight = new THREE.PointLight(0xffffff)
-        // pointLight.position.set(3,2,1)
-        // scene.add(pointLight)
-        // const ambientLight = new THREE.AmbientLight(0xcccccc, 0.5)
-        // scene.add(ambientLight)
-
-        renderer.setSize(window.innerWidth, window.innerHeight - 2)
-        renderer.render(scene, camera)
-
-        // const controls = new OrbitControls(this.camera, this.renderer.domElement)
-        // controls.addEventListener('change', this.handleControl)
-        // this.controls = controls
-
-        document.getElementById('stage').appendChild(renderer.domElement)
-    }
-
-    handleControl = () => {
-        this.renderer.render(this.scene, this.camera)
-    }
-
-    animate = () => {
-        requestAnimationFrame(this.animate)
-        this.mesh.rotation.y += 0.005
-        this.renderer.render(this.scene, this.camera)
-    }
-
-    render() {
-        return (
-            <div id="stage" style={{height: '90%'}}>
-
-            </div>
-        )
-    }
+    return (
+        <>
+            <h1>Your Personal Library</h1>
+            <Table columns={columns} dataSource={data} pagination={false} />
+            <Music audio={audio}/>
+            <Button icon={<IconPlay/>} onClick={() => console.log(audio)}/>
+        </>
+    )
 }
 
 export default RPi
