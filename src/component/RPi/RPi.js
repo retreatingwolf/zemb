1fb0f4768af9d12a5d020e7c203495eeec957bdb
Zhou <1174714790@qq.com>
2023-05-15 13:18:14 +0800

    使用aplayer组件的音乐播放器

--- src/component/RPi/RPi.js
+++ src/component/RPi/RPi.js
@@ -5,64 +5,75 @@ import "./RPi.css"
 import Music from "./Music";
 
 const RPi = () => {
-    const [audio, setAudio] = React.useState([])
-
-    const data = [
+    const [audio, setAudio] = React.useState([
         {
-            key: '1',
             name: 'twice.mp3',
-            size: '2M',
-            updateTime: '2023-01-12 05:13',
-            url: "/resources/audio/twice.mp3"
-        },
-        {
-            key: '2',
-            name: '刘耀文.mp3',
-            size: '2M',
-            updateTime: '2020-01-17 05:31',
-            url: "/resources/audio/刘耀文.mp3"
-        }
-    ];
-
-    const click_play = (record) => {
-        console.log(record.name)
-        console.log(record.url)
-        const new_audio = {
-            name: record.name,
             artist: 'Yourself',
-            url: record.url
+            url: '/resources/audios/twice.mp3'
+        },
+        {
+            name: '刘耀文.mp3',
+            artist: 'Yourself',
+            url: "/resources/audios/刘耀文.mp3"
         }
-        setAudio([].concat(new_audio))
-    }
+    ])
 
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'name',
-        },
-        {
-            title: 'Size',
-            dataIndex: 'size',
-        },
-        {
-            title: 'Update Time',
-            dataIndex: 'updateTime',
-        },
-        {
-            title: 'Play',
-            dataIndex: 'play',
-            render: (text, record) => (
-                <Button icon={<IconPlay/>} theme="borderless" onClick={() => click_play(record)} />
-            ),
-        }
-    ];
+    // const data = [
+    //     {
+    //         key: '1',
+    //         name: 'twice.mp3',
+    //         size: '2M',
+    //         updateTime: '2023-01-12 05:13',
+    //         url: "/resources/audio/twice.mp3"
+    //     },
+    //     {
+    //         key: '2',
+    //         name: '刘耀文.mp3',
+    //         size: '2M',
+    //         updateTime: '2020-01-17 05:31',
+    //         url: "/resources/audio/刘耀文.mp3"
+    //     }
+    // ];
+
+    // const click_play = (record) => {
+    //     console.log(record.name)
+    //     console.log(record.url)
+    //     const new_audio = {
+    //         name: record.name,
+    //         artist: 'Yourself',
+    //         url: record.url
+    //     }
+    //     setAudio([].concat(new_audio))
+    // }
+
+    // const columns = [
+    //     {
+    //         title: 'Name',
+    //         dataIndex: 'name',
+    //     },
+    //     {
+    //         title: 'Size',
+    //         dataIndex: 'size',
+    //     },
+    //     {
+    //         title: 'Update Time',
+    //         dataIndex: 'updateTime',
+    //     },
+    //     {
+    //         title: 'Play',
+    //         dataIndex: 'play',
+    //         render: (text, record) => (
+    //             <Button icon={<IconPlay/>} theme="borderless" onClick={() => click_play(record)} />
+    //         ),
+    //     }
+    // ];
 
     return (
         <>
             <h1>Your Personal Library</h1>
-            <Table columns={columns} dataSource={data} pagination={false} />
+            {/*<Table columns={columns} dataSource={data} pagination={false} />*/}
             <Music audio={audio}/>
-            <Button icon={<IconPlay/>} onClick={() => console.log(audio)}/>
+            {/*<Button icon={<IconPlay/>} onClick={() => console.log(audio)}/>*/}
         </>
     )
 }
